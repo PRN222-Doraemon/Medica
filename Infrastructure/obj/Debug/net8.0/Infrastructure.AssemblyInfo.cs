1988f1280b421b2998332175aead6d7142806329
meewaldor <tieumylam91@gmail.com>
2025-01-22 20:36:03 +0700

    Added IGenericRepository and IUnitOfWork

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f8edf5bdcef6b20262ea509a550326704c06d7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ae90765509a9f8c3c12c75811f268a15a7912f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
