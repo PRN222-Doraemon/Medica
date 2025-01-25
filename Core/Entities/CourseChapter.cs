aaccf39966ffebd63d8877941ca6770cb0bfdd89
meewaldor <tieumylam91@gmail.com>
2025-01-25 17:56:41 +0700

    Added entities

---
+++ Core/Entities/CourseChapter.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Core.Entities
+{
+    public class CourseChapter : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CourseID { get; set; }
+        public Course Course { get; set; }
+        public IEnumerable<Resource> Resources { get; set; } = new List<Resource>();    
+
+    }
+}
---
+++ Core/Entities/Resource.cs
@@ -0,0 +1,27 @@
+﻿using MindSpace.Domain.Entities;
+using System.Runtime.Serialization;
+
+namespace Core.Entities
+{
+    public class Resource : BaseEntity
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string? FileUrl { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime LastUpdatedAt { get; set;} = DateTime.Now;
+        public string Type { get; set; }
+        public int CourseChapterID { get; set; }
+        public CourseChapter CourseChapter { get; set; }
+        public int CreatedByUserID { get; set; }
+        public ApplicationUser CreatedBy { get; set; }
+    }
+
+    public enum ResourceType
+    {
+        [EnumMember(Value = "Lecture Slides")]
+        Slide,
+        [EnumMember(Value = "Online Video")]
+        Video
+    }
+}
