17c67780795e9cb0b8129afb8aab1aa903786c80
anonyfriday <duykimvu75@gmail.com>
2025-01-26 04:02:55 +0700

    #2 add Feedback

--- Core/Entities/Feedback.cs
+++ Core/Entities/Feedback.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,31 @@ using System.Threading.Tasks;
 
 namespace Core.Entities
 {
-    public class Feedback
+    public class Feedback : BaseEntity
     {
+        public decimal Rating { get; set; }
+        public string FeedbackContent { get; set; }
+        public FeedbackStatus Status { get; set; }
 
+
+        // 1 Student - M Feedback
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+
+
+        // 1 Lecturer - M Feedback
+        public int LecturerId { get; set; }
+        public Lecturer Lecturer { get; set; }
+
+
+        // 1 Classroom - M Feedback
+        public int ClassroomId { get; set; }
+        public Classroom Classroom { get; set; }
+    }
+
+    public enum FeedbackStatus
+    {
+        Enabled,
+        Disabled
     }
 }
--- Infrastructure/Configs/FeedbackConfiguration.cs
+++ Infrastructure/Configs/FeedbackConfiguration.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Core.Entities;
+using Core.Entities.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,41 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Configs
 {
-    internal class FeedbackConfiguration
+    internal class FeedbackConfiguration : IEntityTypeConfiguration<Feedback>
     {
+        public void Configure(EntityTypeBuilder<Feedback> builder)
+        {
+            // Properties
+            builder.Property(f => f.Rating)
+             .HasColumnType("decimal(3, 2)")
+             .IsRequired();
+
+            builder.Property(f => f.FeedbackContent)
+             .HasMaxLength(1000)
+             .IsRequired();
+
+            builder.Property(c => c.Status)
+                .HasConversion(
+                s => s.ToString(),
+                s => (FeedbackStatus)Enum.Parse(typeof(FeedbackStatus), s));
+
+            // 1 Lecturer - M Feedback
+            builder.HasOne(c => c.Lecturer)
+                .WithMany(l => l.Feedbacks)
+                .HasForeignKey(c => c.LecturerId)
+                .OnDelete(DeleteBehavior.ClientCascade);
+
+            // 1 Classroom - M Feedback
+            builder.HasOne(c => c.Classroom)
+                .WithMany(l => l.Feedbacks)
+                .HasForeignKey(c => c.ClassroomId)
+                .OnDelete(DeleteBehavior.ClientCascade);
+
+            // 1 Student - M Feedback
+            builder.HasOne(c => c.Student)
+                .WithMany(l => l.Feedbacks)
+                .HasForeignKey(c => c.StudentId)
+                .OnDelete(DeleteBehavior.ClientCascade);
+        }
     }
 }
