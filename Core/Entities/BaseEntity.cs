aaf051bb6ed568e89252f06f5794fd9b3fd3d416
anonyfriday <duykimvu75@gmail.com>
2025-01-25 22:03:44 +0700

    Add applying base entity on all inheriting classes #2

--- Core/Entities/BaseEntity.cs
+++ Core/Entities/BaseEntity.cs
@@ -3,5 +3,7 @@
     public class BaseEntity
     {
         public int Id { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
     }
 }
--- Infrastructure/Persistence/ApplicationDbContext.cs
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Core.Entities.Identity;
+﻿using Core.Entities;
+using Core.Entities.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,18 +7,71 @@ namespace Infrastructure.Persistence
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
     {
+        // ========================
+        // === DbSets
+        // ========================
+
+        private DbSet<Contact> Contacts { get; set; }
+
+        // ========================
+        // === Constructors
+        // ========================
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-        }
+        // ========================
+        // === Methods
+        // ========================
 
+        /// <summary>
+        /// Override the Model Creating 
+        /// </summary>
+        /// <param name="modelBuilder"></param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            // Apply all configurations in the assembly
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            // Apply base entity to all inheriting entities
+            ApplyBaseEntityToDerivedClass(modelBuilder);
+        }
+
+        /// <summary>
+        /// Configuring the base entity
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        private void ApplyBaseEntityToDerivedClass(ModelBuilder modelBuilder)
+        {
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                if (typeof(BaseEntity).IsAssignableFrom(entityType.ClrType) && entityType.ClrType != typeof(BaseEntity))
+                {
+                    // Configure Id
+                    modelBuilder.Entity(entityType.ClrType)
+                        .Property(nameof(BaseEntity.Id))
+                        .IsRequired()
+                        .ValueGeneratedOnAdd()
+                        .UseIdentityColumn();
+
+                    // Configure CreatedAt
+                    modelBuilder.Entity(entityType.ClrType)
+                        .Property(nameof(BaseEntity.CreateAt))
+                        .IsRequired()
+                        .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                        .ValueGeneratedOnAdd();
+
+                    // Configure UpdatedAt
+                    modelBuilder.Entity(entityType.ClrType)
+                        .Property(nameof(BaseEntity.UpdateAt))
+                        .IsRequired()
+                        .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                        .ValueGeneratedOnAddOrUpdate();
+                }
+            }
         }
     }
 }
