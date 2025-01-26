7705c787414b0ace809277a92b0294952c2d0f04
Jeralith <tuananh2634@gmail.com>
2025-01-26 10:36:40 +0700

    add entities n config

---
+++ Infrastructure/Configs/OrderConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configs
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.Property(o => o.StudentId)
+                .IsRequired();
+            builder.Property(o => o.OrderTime)
+                .IsRequired();
+            builder.Property(o => o.Status)
+                .HasConversion(s => s.ToString(),
+                s => (OrderStatus) Enum.Parse(typeof(OrderStatus), s));
+
+            builder.Property(o => o.PaymentIntentId)
+                .IsRequired();
+            builder.Property(o => o.TotalPrice)
+                .IsRequired();
+
+            //1 Student - M Order
+            builder.HasOne(s => s.Student)
+                .WithMany(o => o.Order)
+                .HasForeignKey(s.StudentId)
+                .OnDelete(DeleteBehaviour.CilentCascade);
+
+        }
+    }
+}
