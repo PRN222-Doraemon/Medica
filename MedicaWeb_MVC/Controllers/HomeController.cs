ef3edaf53df41f2b5b65ea6a4aef2af0fc297f10
anonyfriday <duykimvu75@gmail.com>
2025-02-07 01:14:31 +0700

    Add Navbar and Reusable SearchBar #1

--- MedicaWeb_MVC/Controllers/HomeController.cs
+++ MedicaWeb_MVC/Controllers/HomeController.cs
@@ -6,15 +6,38 @@ namespace MedicaWeb_MVC.Controllers
 {
     public class HomeController : Controller
     {
+        // =========================
+        // === Fields & Props
+        // =========================
+
         private readonly ILogger<HomeController> _logger;
+
+        // =========================
+        // === Constructors
+        // =========================
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
+        // =========================
+        // === Methods
+        // =========================
+
         public IActionResult Index()
         {
+            // Define Narbar
+            //var navbarVM = new NavbarVM()
+            //{
+            //    MenuItems = new List<NavbarItemVM>
+            //    {
+            //         new NavbarItemVM { Name = "Courses", Endpoint = Url.Action("Index", "Courses") ?? string.Empty },
+            //        new NavbarItemVM { Name = "News", Endpoint = Url.Action("Index", "News") ?? string.Empty},
+            //        new NavbarItemVM { Name = "My Learnings", Endpoint = Url.Action("Index", "Mylearning") ?? string.Empty }
+            //    }
+            //};
+
             return View();
         }
 
