0ecb512a8055bff3d6a02cdb49620c160e2242db
anonyfriday <duykimvu75@gmail.com>
2025-03-31 08:11:49 +0700

    Add Order Management

--- Core/Specifications/Orders/OrderParams.cs
+++ Core/Specifications/Orders/OrderParams.cs
@@ -2,13 +2,13 @@
 
 namespace Core.Specifications.Orders
 {
-    public class OrderParams
+    public class OrderParams : PagingParams
     {
         public int? StudentID { get; set; }
         public int? CourseID { get; set; }
         public OrderStatus? Status { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
-
+        public string? Search { get; set; }
     }
 }
